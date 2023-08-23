cfb7f0bcdb571885ef89225b049fca1ddc0e9009
Lucas01-hash <lucassdeveloper@gmail.com>
2023-08-23 00:16:44 -0300

    fix: adjust page bill

--- src/renderer/src/pages/Command/styles.ts
+++ src/renderer/src/pages/Command/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  width: 100%;
+  width: calc(100vw - 18rem);
   padding: 36px;
   display: flex;
   flex-direction: column;
