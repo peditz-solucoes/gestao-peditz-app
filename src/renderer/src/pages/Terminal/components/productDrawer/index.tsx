5e7fd137c6df39d6c1a6f4e77ed9493752718e5f
Matheu Santos <Matheus.souza@acad.ifma.edu.br>
2023-08-28 08:41:20 -0300

    fix terminal

--- src/renderer/src/pages/Terminal/components/productDrawer/index.tsx
+++ src/renderer/src/pages/Terminal/components/productDrawer/index.tsx
@@ -1,122 +1,109 @@
-import React, { useEffect, useState } from "react";
-import {
-  Button,
-  Drawer,
-  Form,
-  Input,
-  InputNumber,
-  Skeleton,
-  Typography,
-} from "antd";
-import api from "../../../../services/api";
-import { Product } from "../../../../types";
-import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
-import { CheckBoxComplement } from "../CheckBoxComplement";
-import { useTerminal } from "../../../../hooks/useTerminal";
-import { NumberComplement } from "../NumberComplement";
+import React, { useEffect, useState } from 'react'
+import { Button, Drawer, Form, Input, InputNumber, Skeleton, Typography } from 'antd'
+import api from '../../../../services/api'
+import { Product } from '../../../../types'
+import { MinusOutlined, PlusOutlined } from '@ant-design/icons'
+import { CheckBoxComplement } from '../CheckBoxComplement'
+import { useTerminal } from '../../../../hooks/useTerminal'
+import { NumberComplement } from '../NumberComplement'
 
 interface ProductDrawerProps {
-  visible: string;
-  onClose: () => void;
+  visible: string
+  onClose: () => void
 }
 
 interface ComplementItem {
-  id: string;
-  title: string;
-  price: string;
-  order: number;
-  min_value: number;
-  max_value: number;
+  id: string
+  title: string
+  price: string
+  order: number
+  min_value: number
+  max_value: number
 }
 
 interface ProductComplement {
-  id: string;
-  title: string;
-  order: number;
-  active: boolean;
-  input_type: string;
-  business_rules: string;
-  max_value: number;
-  min_value: number;
-  product: string;
-  complement_items: ComplementItem[];
+  id: string
+  title: string
+  order: number
+  active: boolean
+  input_type: string
+  business_rules: string
+  max_value: number
+  min_value: number
+  product: string
+  complement_items: ComplementItem[]
 }
 
 interface DataToAdd {
-  product_id: string;
-  quantity: number;
-  product_title: string;
-  notes: string;
+  product_id: string
+  quantity: number
+  product_title: string
+  notes: string
   complements: {
-    complement_id: string;
-    complement_title: string;
+    complement_id: string
+    complement_title: string
     items: {
-      item_id: string;
-      item_title: string;
-      quantity: number;
-    }[];
-  }[];
+      item_id: string
+      item_title: string
+      quantity: number
+    }[]
+  }[]
 }
 
-export const ProductDrawer: React.FC<ProductDrawerProps> = ({
-  onClose,
-  visible,
-}) => {
-  const [product, setProduct] = useState<Product>({} as Product);
-  const [productLoading, setProductLoading] = useState(false);
-  const [productComplements, setProductComplements] = useState<
-    ProductComplement[]
-  >([]);
-  const { setCart, cart } = useTerminal();
-  const [dataToadd, setDataToAdd] = useState<DataToAdd>({} as DataToAdd);
-  const quantityInput = React.useRef<HTMLInputElement>(null);
-  const buttonRef = React.useRef<HTMLButtonElement>(null);
+export const ProductDrawer: React.FC<ProductDrawerProps> = ({ onClose, visible }) => {
+  const [product, setProduct] = useState<Product>({} as Product)
+  const [productLoading, setProductLoading] = useState(false)
+  const [productComplements, setProductComplements] = useState<ProductComplement[]>([])
+  const { setCart, cart } = useTerminal()
+  const [dataToadd, setDataToAdd] = useState<DataToAdd>({} as DataToAdd)
+  const quantityInput = React.useRef<HTMLInputElement>(null)
+  const buttonRef = React.useRef<HTMLButtonElement>(null)
   const fetchComplements = (id: string) => {
     api
-      .get(`/product-complement/?product=${id}&active=true`)
+      .get(`/product-complement/?products=${id}&active=true`)
       .then((response) => {
-        setProductComplements(response.data);
+        setProductComplements(response.data)
       })
       .catch((error) => {
-        console.log(error);
-      });
-  };
+        console.log(error)
+      })
+  }
   const fetchProduct = (id: string) => {
-    setProductLoading(true);
+    setProductLoading(true)
     api
       .get(`/product/${id}/`)
       .then((response) => {
-        setProduct(response.data);
-        fetchComplements(response.data.id);
+        setProduct(response.data)
+        fetchComplements(response.data.id)
         setDataToAdd({
           product_id: response.data.id,
-          quantity: response.data.type_of_sale === "UN" ? 1 : 0,
-          notes: "",
+          quantity: response.data.type_of_sale === 'UN' ? 1 : 0,
+          notes: '',
           product_title: response.data.title,
-          complements: [],
-        });
+          complements: []
+        })
         if (quantityInput.current) {
-          quantityInput.current.focus();
+          quantityInput.current.focus()
         }
-        if (response.data.type_of_sale === "UN" && buttonRef.current) {
-          buttonRef.current.focus();
+        if (response.data.type_of_sale === 'UN' && buttonRef.current) {
+          buttonRef.current.focus()
         }
       })
       .catch((error) => {
-        console.log(error);
+        console.log(error)
       })
       .finally(() => {
-        setProductLoading(false);
-      });
-  };
+        setProductLoading(false)
+      })
+  }
 
   useEffect(() => {
-    setProduct({} as Product);
-    setProductComplements([]);
+    setProduct({} as Product)
+    setProductComplements([])
     if (visible) {
-      fetchProduct(visible);
+      fetchProduct(visible)
     }
-  }, [visible]);
+  }, [visible])
   return (
     <Drawer
       title={
@@ -127,7 +114,7 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
         )
       }
       placement="right"
-      size={"large"}
+      size={'large'}
       onClose={onClose}
       open={Boolean(visible)}
       closable={false}
@@ -136,9 +123,9 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
         layout="vertical"
         size="large"
         style={{
-          display: "flex",
-          flexDirection: "column",
-          height: "100%",
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%'
         }}
       >
         <Form.Item label="Observação">
@@ -146,12 +133,12 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
             size="large"
             value={dataToadd.notes}
             onChange={(e) => {
-              setDataToAdd({ ...dataToadd, notes: e.target.value });
+              setDataToAdd({ ...dataToadd, notes: e.target.value })
             }}
           />
         </Form.Item>
         {productComplements.map((complement) => {
-          return complement.input_type !== "number" ? (
+          return complement.input_type !== 'number' ? (
             <CheckBoxComplement
               key={complement.id}
               selcteds={
@@ -162,7 +149,7 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
               onChange={(items) => {
                 const complementIndex = dataToadd.complements.findIndex(
                   (x) => x.complement_id === complement.id
-                );
+                )
                 if (complementIndex === -1) {
                   setDataToAdd({
                     ...dataToadd,
@@ -174,26 +161,23 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
                         items: items.map((i) => ({
                           item_id: i,
                           item_title:
-                            complement.complement_items.find((x) => x.id === i)
-                              ?.title || "",
-                          quantity: 1,
-                        })),
-                      },
-                    ],
-                  });
+                            complement.complement_items.find((x) => x.id === i)?.title || '',
+                          quantity: 1
+                        }))
+                      }
+                    ]
+                  })
                 } else {
-                  const newComplements = dataToadd.complements;
+                  const newComplements = dataToadd.complements
                   newComplements[complementIndex].items = items.map((i) => ({
                     item_id: i,
-                    item_title:
-                      complement.complement_items.find((x) => x.id === i)
-                        ?.title || "",
-                    quantity: 1,
-                  }));
+                    item_title: complement.complement_items.find((x) => x.id === i)?.title || '',
+                    quantity: 1
+                  }))
                   setDataToAdd({
                     ...dataToadd,
-                    complements: newComplements,
-                  });
+                    complements: newComplements
+                  })
                 }
               }}
               title={complement.title}
@@ -201,7 +185,7 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
               items={complement.complement_items.map((item) => ({
                 id: item.id,
                 title: item.title,
-                price: Number(item.price),
+                price: Number(item.price)
               }))}
             />
           ) : (
@@ -215,12 +199,12 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
                   dataToadd.complements
                     .find((x) => x.complement_id === complement.id)
                     ?.items.find((x) => x.item_id === item.id)?.quantity || 0,
-                item_price: item.price,
+                item_price: item.price
               }))}
               onChange={(value) => {
                 const complementIndex = dataToadd.complements.findIndex(
                   (x) => x.complement_id === complement.id
-                );
+                )
                 if (complementIndex === -1) {
                   setDataToAdd({
                     ...dataToadd,
@@ -229,43 +213,47 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
                       {
                         complement_id: complement.id,
                         complement_title: complement.title,
-                        items: value.map((i) => ({
-                          item_id: i.item_id,
-                          item_title: i.item_title,
-                          quantity: i.quantity,
-                        })).filter(x => x.quantity > 0),
-                      },
-                    ],
-                  });
+                        items: value
+                          .map((i) => ({
+                            item_id: i.item_id,
+                            item_title: i.item_title,
+                            quantity: i.quantity
+                          }))
+                          .filter((x) => x.quantity > 0)
+                      }
+                    ]
+                  })
                 } else {
-                  const newComplements = dataToadd.complements;
-                  newComplements[complementIndex].items = value.map((i) => ({
-                    item_id: i.item_id,
-                    item_title: i.item_title,
-                    quantity: i.quantity,
-                  })).filter(x => x.quantity > 0);
+                  const newComplements = dataToadd.complements
+                  newComplements[complementIndex].items = value
+                    .map((i) => ({
+                      item_id: i.item_id,
+                      item_title: i.item_title,
+                      quantity: i.quantity
+                    }))
+                    .filter((x) => x.quantity > 0)
                   setDataToAdd({
                     ...dataToadd,
-                    complements: newComplements,
-                  });
+                    complements: newComplements
+                  })
                 }
               }}
             />
-          );
+          )
         })}
         <div
           style={{
-            display: "flex",
-            gap: "1rem",
-            marginTop: "auto",
+            display: 'flex',
+            gap: '1rem',
+            marginTop: 'auto'
           }}
         >
-          {product.type_of_sale === "UN" ? (
+          {product.type_of_sale === 'UN' ? (
             <div
               style={{
-                display: "flex",
-                alignItems: "center",
-                gap: "0.7rem",
+                display: 'flex',
+                alignItems: 'center',
+                gap: '0.7rem'
               }}
             >
               <Button
@@ -273,26 +261,21 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
                   if (dataToadd.quantity > 1) {
                     setDataToAdd({
                       ...dataToadd,
-                      quantity: dataToadd.quantity - 1,
-                    });
+                      quantity: dataToadd.quantity - 1
+                    })
                   }
                 }}
               >
                 <MinusOutlined />
               </Button>
-              <InputNumber
-                value={dataToadd.quantity}
-                readOnly
-                controls={false}
-                size="large"
-              />
+              <InputNumber value={dataToadd.quantity} readOnly controls={false} size="large" />
 
               <Button
                 onClick={() => {
                   setDataToAdd({
                     ...dataToadd,
-                    quantity: dataToadd.quantity + 1,
-                  });
+                    quantity: dataToadd.quantity + 1
+                  })
                 }}
               >
                 <PlusOutlined />
@@ -303,11 +286,11 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
               ref={quantityInput}
               value={dataToadd.quantity}
               onChange={(value) => {
-                setDataToAdd({ ...dataToadd, quantity: value || 0 });
+                setDataToAdd({ ...dataToadd, quantity: value || 0 })
               }}
               style={{
-                width: "40%",
-                height: "50px",
+                width: '40%',
+                height: '50px'
               }}
               controls={false}
               size="large"
@@ -315,8 +298,8 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
           )}
           <Button
             onClick={() => {
-              setCart([...cart, dataToadd]);
-              onClose();
+              setCart([...cart, dataToadd])
+              onClose()
             }}
             ref={buttonRef}
             type="primary"
@@ -328,5 +311,5 @@ export const ProductDrawer: React.FC<ProductDrawerProps> = ({
         </div>
       </Form>
     </Drawer>
-  );
-};
+  )
+}
