5650e1d310e17ce047d9f5dfbee98c508d8d3325
Matheu Santos <Matheus.souza@acad.ifma.edu.br>
2023-10-03 15:26:23 -0300

    fix prdoucts

--- src/renderer/src/services/api.ts
+++ src/renderer/src/services/api.ts
@@ -2,9 +2,9 @@ import axios, { AxiosRequestHeaders } from 'axios'
 import { getToken } from './auth'
 
 const api = axios.create({
-  // baseURL: 'https://api.peditz.com/api/v1/',
+  baseURL: 'https://api.peditz.com/api/v1/',
   // baseURL: 'http://localhost:8000/api/v1/',
-  baseURL: 'https://api-hml.peditz.com/api/v1/',
+  // baseURL: 'https://api-hml.peditz.com/api/v1/',
   headers: {
     'Content-Type': 'application/json;charset=UTF-8'
   }
